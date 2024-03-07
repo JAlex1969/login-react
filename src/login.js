21089843c398758dbf9434cdb5af9afcdefd680e
JAlex1969 <josealex1969@gmail.com>
2024-03-07 13:50:03 +0000

    Inicial

---
+++ src/login.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = (props) => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [emailError, setEmailError] = useState("")
+    const [passwordError, setPasswordError] = useState("")
+
+    const navigate = useNavigate();
+
+    const onButtonClick = () => {
+
+        // Set initial error values to empty
+        setEmailError("")
+        setPasswordError("")
+
+        // Check if the user has entered both fields correctly
+        if ("" === email) {
+            setEmailError("Please enter your email")
+            return
+        }
+
+        if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+            setEmailError("Please enter a valid email")
+            return
+        }
+
+        if ("" === password) {
+            setPasswordError("Please enter a password")
+            return
+        }
+
+        if (password.length < 7) {
+            setPasswordError("The password must be 8 characters or longer")
+            return
+        }
+
+        logIn();
+    }
+
+    // Log in a user using email and password
+    const logIn = () => {
+        fetch(`http://localhost:4000/login?email=${email}`)
+            .then(res => res.json())
+            .then((result) => {
+                if (result.length == 0) {
+                    window.alert("O e-mail não existe!");
+                } else if (email === result[0].Email && password !== result[0].Password) {
+                    window.alert("Password inválida!!");
+                } else {
+                    localStorage.setItem("user", JSON.stringify({ email, token: result.token }))
+                    props.setLoggedIn(true)
+                    props.setEmail(email)
+                    navigate("/")
+                }
+            });
+    }
+
+    return <div className={"mainContainer"}>
+        <div className={"titleContainer"}>
+            <div>Login</div>
+        </div>
+        <br />
+        <div className={"inputContainer"}>
+            <input
+                value={email}
+                placeholder="Enter your email here"
+                onChange={ev => setEmail(ev.target.value)}
+                className={"inputBox"} />
+            <label className="errorLabel">{emailError}</label>
+        </div>
+        <br />
+        <div className={"inputContainer"}>
+            <input
+                value={password}
+                type="password"
+                placeholder="Enter your password here"
+                onChange={ev => setPassword(ev.target.value)}
+                className={"inputBox"} />
+            <label className="errorLabel">{passwordError}</label>
+        </div>
+        <br />
+        <div className={"inputContainer"}>
+            <input
+                className={"inputButton"}
+                type="button"
+                onClick={onButtonClick}
+                value={"Log in"} />
+        </div>
+    </div>
+}
+
+export default Login
